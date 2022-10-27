ec53837d5ec6732eb6c6e6ebf2a8c555900df828
Tea Antila <teaa@uef.fi>
2022-10-27 13:29:10 +0300

    npm updated, initial page for the application

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+//Decorator that gives normal classess some more extra powers (our class AppComponent is now an angular component)
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//Class with title property
 export class AppComponent {
-  title = 'client';
+  title = 'My awesome app';
 }
